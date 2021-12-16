974345df86e2d86a7ab3d419fedcc6836aba1193
Debapriya Majumder <guddumajumder75@gmail.com>
2021-12-16 23:35:46 +0530

    Add quiz prompt

--- index.js
+++ index.js
@@ -6,9 +6,11 @@
  * 
  */
 
+const { Quiz } = require('enquirer');
 const init = require('./utils/init');
 const cli = require('./utils/cli');
 const log = require('./utils/log');
+const alert = require('terminal-alerts');
 
 const input = cli.input;
 const flags = cli.flags;
@@ -20,6 +22,31 @@ const { clear , debug } = flags;
 
     init({clear});
     input.includes(`help`) && cli.showHelp(0);
+
+    const prompt = new Quiz({
+        message : `How many bitcoin will ever be created ?`,
+        choices : [`2 billion` , `21 billion` , `21 million` , `10 million`],
+        correctChoice : 2
+    });
+
+    prompt
+        .run()
+        .then(answer => {
+        if (answer.correct) {
+            alert({
+                type: `success`,
+                name : ` CORRECT `,
+                msg : `You selected the correct choice`
+            });
+        } else {
+            alert({
+                type: `error`,
+                name : ` WRONG `,
+                msg : `Correct answer is ${answer.correctAnswer}`
+            });
+        }
+    })
+    .catch(console.error);
 
     debug && log(flags);
 
